b9011bbe1cefc1b2272a88020ceb4166cc90a189
KannanPalani57 <kannangreener@gmail.com>
2020-04-26 21:13:16 +0530

    Simple Class & Object in Java

---
+++ Day - 7/classandobject.java
@@ -0,0 +1,13 @@
+class Class{
+
+void hello(){
+  System.out.println("Hello there");
+}
+
+public static void main(String[] args){
+  Class obj = new Class();
+
+  obj.hello(); //calling the function with object.s
+}
+
+}
