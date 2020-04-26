9386b9af5ee3f48ffb7b67d9bb290c54d9a277b7
KannanPalani57 <kannangreener@gmail.com>
2020-04-26 21:56:21 +0530

    Loops in Java

---
+++ Day 1,2& 3,4/for.java
@@ -0,0 +1,12 @@
+class For{
+public static void main(String[] args){
+
+  for(int i = 0; i < 5; i++){
+     System.out.println("Current Value is"+i);
+}
+
+}
+
+
+
+}
---
+++ Day 1,2& 3,4/while.java
@@ -0,0 +1,16 @@
+class While{
+public static void main(String[] args){
+
+  int i = 0;
+
+ while(i <=5){
+  System.out.println("i = "+i);
+ i++;
+  
+}
+
+}
+
+
+
+}
